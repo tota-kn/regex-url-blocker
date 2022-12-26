bab8d15da788039982bd2c0f59a0ecffd955ec93
iwatos <nonbiriactive@gmail.com>
2022-12-26 22:24:27 +0900

    update

---
+++ background.ts
@@ -0,0 +1,31 @@
+import { getSyncStorage, getCurrentTab } from "./util"
+
+const block = async (tab: chrome.tabs.Tab, urls: string[]) => { 
+    for (const url of urls) {
+        const result = tab.url?.match(url)
+        if (result !== null) {
+            updateTab(tab, await getSyncStorage("redirectUrl"))
+            break;
+        }
+    }
+}
+
+const updateTab = (tab: chrome.tabs.Tab, url: string) => {
+    if(tab.id === undefined){
+        return
+    }
+    chrome.tabs.update(tab.id, { url })
+}
+
+chrome.tabs.onUpdated.addListener(async () => {
+    const currentTab = await getCurrentTab()
+    const blockUrls = await getSyncStorage("blockUrls")
+    // block(currentTab, blockUrls);
+    block(currentTab, [".*youtube.*"]);
+})
+
+
+
+
+
+
---
+++ util.ts
@@ -0,0 +1,23 @@
+const getSyncStorage = async(key: string) => {
+    const result = await chrome.storage.sync.get(key)
+    return result[key]
+}
+
+const setSyncStorage = async(key: string, value: any) => {
+    return await chrome.storage.sync.set({[key]:value})
+}
+
+const getCurrentTab = async () => {
+    const tabs = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+        currentWindow: true
+    });
+    return tabs[0];
+}
+
+export {
+    getSyncStorage,
+    setSyncStorage,
+    getCurrentTab
+}
